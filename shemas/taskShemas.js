aa45352c0eca70e85281bed91cb4a40ed5dd978a
Dadayat <136356333+Dadayat@users.noreply.github.com>
2024-06-06 10:49:54 +0300

    update taskShema

--- shemas/taskShemas.js
+++ shemas/taskShemas.js
@@ -1,10 +1,22 @@
 import Joi from "joi";
 
-export const taskShema = Joi.object({
-    title: Joi.string(), required(),
-    description: Joi.string(), required(),
-    labelColor: Joi.string(), required(),
-    description: Joi.string(), required(),
-    deadline: Joi.date(), required(),
-    priority:  Joi.string(), required(),
-})
+export const createTaskShema = Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    labelColor: Joi.string().required(),
+    description: Joi.string().required(),
+    deadline: Joi.date().required(),
+    priority:  Joi.string().required(),
+});
+
+export const updateTaskShema = Joi.object({
+    title: Joi.string(),
+    description: Joi.string(),
+    labelColor: Joi.string(), 
+    description: Joi.string(), 
+    deadline: Joi.date(), 
+    priority:  Joi.string(), 
+})  
+.min(1)
+.messages({ "object.min": "Body must have at least one field" });
+
