bb244a7f8636113f194e397d22516af58cc239e0
serhiyserhiychuk <141840178+serhiyserhiychuk@users.noreply.github.com>
2024-06-06 15:27:47 -0400

    add trackerRouter

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authRouter from "./authRouter.js";
+import trackerRouter from "./trackerRouter.js";
 
 const router = express.Router();
 
 // router.use("/welcome")
 router.use("/auth", authRouter);
-// router.use("/home", trackerRouter)
+router.use("/home", trackerRouter);
 
 export default router;
